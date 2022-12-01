46fef1ac5cdabb69e089d27c7ceb5c0abcc05fea
Christopher Kruger <ckruger@stevens.edu>
2022-11-30 23:27:37 -0500

    Updated basic menu and added a second song

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <Windows.h>
 #include <mmsystem.h>
+#include <iomanip>
+#include <ios>
+
 #pragma comment(lib, "Winmm.lib")
+
 using namespace std;
 
-int main() {
+void introGUI()
+{
+	cout << "**********" << endl;
+	cout << "Welcome to our project!";
+	cout << "Please type in the number of the song you want to play: " << endl;
+	cout << left << setfill('.') << setw(30) << "1 for"  << right << setfill('.') << setw(20) << "'Follow You' by Imagine Dragons" << endl;
+	cout << left << setfill('.') << setw(30) << "2 for" << right << setfill('.') << setw(20) << "'good 4 u' by Olivia Rodrigo" << endl;
+}
 
-	PlaySound(TEXT("Song.wav"), NULL, SND_ASYNC);
+int main() {
+	introGUI();
+	PlaySound(TEXT("Follow You.wav"), NULL, SND_ASYNC); 
 	Sleep(5000);
 	PlaySound(NULL, 0, 0);
 
