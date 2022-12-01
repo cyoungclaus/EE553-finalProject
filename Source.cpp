12d5d9838be37f2e46af745e7abbcb19a4b2ca49
Christopher Kruger <ckruger@stevens.edu>
2022-11-30 23:32:39 -0500

    Playing two songs now

--- Source.cpp
+++ Source.cpp
@@ -11,7 +11,7 @@ using namespace std;
 void introGUI()
 {
 	cout << "**********" << endl;
-	cout << "Welcome to our project!";
+	cout << "Welcome to our project!" << endl;;
 	cout << "Please type in the number of the song you want to play: " << endl;
 	cout << left << setfill('.') << setw(30) << "1 for"  << right << setfill('.') << setw(20) << "'Follow You' by Imagine Dragons" << endl;
 	cout << left << setfill('.') << setw(30) << "2 for" << right << setfill('.') << setw(20) << "'good 4 u' by Olivia Rodrigo" << endl;
@@ -20,6 +20,9 @@ void introGUI()
 int main() {
 	introGUI();
 	PlaySound(TEXT("Follow You.wav"), NULL, SND_ASYNC); 
+	Sleep(5000);
+	PlaySound(NULL, 0, 0);
+	PlaySound(TEXT("good 4 u.wav"), NULL, SND_ASYNC);
 	Sleep(5000);
 	PlaySound(NULL, 0, 0);
 
