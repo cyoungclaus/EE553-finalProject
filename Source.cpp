88ade9ea97dfece1687d23c03966c3ddabb91158
Christopher Kruger <ckruger@stevens.edu>
2022-12-05 20:37:29 -0500

    First addition of a function to play the song, not currently working

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,16 @@
 #include <mmsystem.h>
 #include <iomanip>
 #include <ios>
+#include <mciapi.h>
+
 
 #pragma comment(lib, "Winmm.lib")
 
 using namespace std;
 
 int choice;
+string song;
+
 void introGUI()
 {
 	cout << "**********" << endl;
@@ -16,18 +20,36 @@ void introGUI()
 	cout << "Please type in the number of the song you want to play: " << endl;
 	cout << left << setfill('.') << setw(30) << "1 for" << right << setfill('.') << setw(20) << "'Follow You' by Imagine Dragons" << endl;
 	cout << left << setfill('.') << setw(30) << "2 for" << right << setfill('.') << setw(20) << "'good 4 u' by Olivia Rodrigo" << endl;
+	cout << "**********" << endl;
+	cout << "For playback controls: " << endl;
+	cout << left << setfill('.') << setw(30) << "3 for" << right << setfill('.') << setw(20) << "Stopping the currently playing song" << endl;
+
+}
+
+void playingSong(const string audioFileName)
+{
+	string song = audioFileName;
+	PlaySound(TEXT(audioFileName), NULL, SND_ASYNC);
+
 }
 void decision()
 {
 	switch (choice) {
 	case 1:
-		PlaySound(TEXT("Follow You.wav"), NULL, SND_ASYNC);
+		//PlaySound(TEXT("FollowYou.wav"), NULL, SND_ASYNC);
+		//mciSendString("play \"FollowYou.wav\" repeat", NULL, 0, NULL);
+		playingSong("Follow You.wav");
 		cout << "Playing 'Follow You' by Imagine Dragons!" << endl;
 		break;
 
 	case 2:
 		PlaySound(TEXT("good 4 u.wav"), NULL, SND_ASYNC);
 		cout << "Playing 'good 4 u' by Olivia Rodrigo!" << endl;
+		break;
+
+	case 3:
+		PlaySound(NULL, 0, 0);
+		cout << "Stopped the song being played!" << endl;
 		break;
 
 	default:
@@ -44,6 +66,8 @@ int main() {
 		decision();
 
 	}
+
+
 
 	/*
 	PlaySound(TEXT("Follow You.wav"), NULL, SND_ASYNC);
